be129968f7ee571f77948ee209b6a5100af174f9
miguelherraez <miguelherraez@users.noreply.github.com>
2019-05-22 23:10:32 +0200

    .

---
+++ PlantsVSZombies with grafics/src/Usuarios/Partidas.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Usuarios;
+
+import java.io.*;
+import plantsvszombies.Dificultad;
+/**
+ *
+ * @author Usuario
+ */
+public class Partidas implements Serializable{
+    
+    /**
+     * Cantidad de puntos obtenidos en una partida
+     */
+    private final int pts;
+    /**
+     * Dificultad de la partida
+     */
+    private final Dificultad diff;
+    /**
+     * Partida ganada o no, si es true la partida esta ganada, si no, esta perdida.
+     */
+    private final boolean p_ganada;
+    /**
+     * 
+     * @param diff
+     * @param pts
+     * @param ganada 
+     */
+    public Partidas(Dificultad diff, int pts, boolean ganada){
+        this.diff=diff;
+        this.pts=pts;
+        this.p_ganada=ganada;
+    }
+}
---
+++ PlantsVSZombies with grafics/src/Usuarios/Usuario.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Usuarios;
+
+import java.io.*;
+import java.util.*;
+import plantsvszombies.Dificultad;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Usuario implements Serializable {
+    
+    /**  
+     * Nombre identificativo del usuario 
+     */
+    private final String nombre;
+    /**
+     * DNI del usuario
+     */
+    private final String DNI;
+    /**
+     * Puntos totales que el usuario ha obtenido
+     */
+    private int pts;
+    /**
+     * Partidas jugadas por el usuario
+     */
+    private ArrayList<Partidas> p_jugadas;
+    /**
+     * Numero de partidas ganadas
+     */
+    private int p_ganadas;
+    /**
+     * 
+     * @param diff
+     * @param pts
+     * @param ganada 
+     */
+    public void PartidaJugada(Dificultad diff, int pts, boolean ganada){
+        p_jugadas.add(new Partidas(diff,pts,ganada));
+        this.pts+=pts;
+        if(ganada){
+            p_ganadas+=1;
+        }
+    }
+    /**
+     * 
+     * @return 
+     */
+    public String getDNI(){
+        return DNI;
+    }
+    /**
+     * 
+     * @return 
+     */
+    public String getNombre(){
+        return nombre;
+    }
+    /**
+     * 
+     * @return 
+     */
+    public int getPts(){
+        return pts;
+    }
+    /**
+     * 
+     * @return 
+     */
+    public ArrayList getP_jugadas(){
+        return p_jugadas;
+    }
+    /**
+     * 
+     * @return 
+     */
+    public int getP_ganadas(){
+        return p_ganadas;
+    }
+    /**
+     * 
+     * @param nomb
+     * @param dni 
+     */
+    public Usuario(String nomb,String dni){
+        this.nombre=nomb;
+        this.DNI=dni;
+    }
+}
